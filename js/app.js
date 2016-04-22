dc04eb3d0d1cce54bca95ab60e45e79a1b3a3b43
Johnny Luangphasy <johnny@jluangphasy.com>
2016-04-22 13:36:26 -0700

    Close #1

--- js/app.js
+++ js/app.js
@@ -11,20 +11,20 @@ var ImgObj = function (pImgFileLoc){
 
 //gobal vars
 var imgFileLocations = [
-  "img/658668main_pia15685-full_full.jpg",
-  "img/658682main_pia15686_full.jpg",
-  "img/667877main_Grotzinger-1PIA15690_800-600.jpg",
-  "img/675225main_pia16029-43_800-600.jpg",
-  "img/676026main_pia16052-color-43_800-600.jpg",
-  "img/676039main_pia16053-color-43_800-600.jpg",
-  "img/678276main_pia16077-43_800-600.jpg",
-  "img/734386main_pia16768-43_800-600.jpg",
-  "img/PIA17766-800x600.jpg",
-  "img/PIA17946-800x600.jpg",
-  "img/PIA18083-800x600.jpg",
-  "img/PIA19803-800x600.jpg",
-  "img/PSP_008579_9020_descent_800-600.jpg",
-  "img/glacialcraters_mro.jpg"
+  'img/658668main_pia15685-full_full.jpg',
+  'img/658682main_pia15686_full.jpg',
+  'img/667877main_Grotzinger-1PIA15690_800-600.jpg',
+  'img/675225main_pia16029-43_800-600.jpg',
+  'img/676026main_pia16052-color-43_800-600.jpg',
+  'img/676039main_pia16053-color-43_800-600.jpg',
+  'img/678276main_pia16077-43_800-600.jpg',
+  'img/734386main_pia16768-43_800-600.jpg',
+  'img/PIA17766-800x600.jpg',
+  'img/PIA17946-800x600.jpg',
+  'img/PIA18083-800x600.jpg',
+  'img/PIA19803-800x600.jpg',
+  'img/PSP_008579_9020_descent_800-600.jpg',
+  'img/glacialcraters_mro.jpg'
 ];
 var didReset = true;
 var whichSitesNow = [0 , 1];
@@ -42,8 +42,8 @@ var VoteTracker = function() {
     whichSitesNow = this.randomPickTwo();
     this.point1 = this.findTagOne.appendChild(document.createElement('div'));
     this.point2 = this.findTagTwo.appendChild(document.createElement('div'));
-    this.point1.innerHTML = "<img src=\'" + imgArray[whichSitesNow[0]].fileNameForImg + "\' alt='The first site to consider' title='This is the first landing site to consider for a mission to Mars. Click on the image to vote in favor.' \>";
-    this.point2.innerHTML = "<img src=\'" + imgArray[whichSitesNow[1]].fileNameForImg + "\' alt='The second site to consider' title='This is the second landing site to consider for a mission to Mars. Click on the image to vote in favor.' \>";
+    this.point1.innerHTML = '<img src="' + imgArray[whichSitesNow[0]].fileNameForImg + '" alt="The first site to consider" title="This is the first landing site to consider for a mission to Mars. Click on the image to vote in favor.">';
+    this.point2.innerHTML = '<img src="' + imgArray[whichSitesNow[1]].fileNameForImg + '" alt="The second site to consider" title="This is the second landing site to consider for a mission to Mars. Click on the image to vote in favor.">';
     this.point1.addEventListener('click', this.handleImgClicks);
     this.point2.addEventListener('click', this.handleImgClicks);
 
@@ -52,16 +52,16 @@ var VoteTracker = function() {
 };
 
 VoteTracker.prototype.makeChart = function () {
-  var ctx = document.getElementById('voteChart').getContext("2d");
+  var ctx = document.getElementById('voteChart').getContext('2d');
   var data = { //beware, hardcoded to 14 landing sites
     labels: ['Site1', 'Site2', 'Site3', 'Site4', 'Site5', 'Site6', 'Site7', 'Site8','Site9', 'Site10', 'Site11', 'Site12', 'Site13', 'Site14'],
     datasets: [
       {
-        label: "Voting Set One, Round One",
-        fillColor: "#0d0d0d",
-        strokeColor: "#5ab8f3",
-        highlightFill: "#a0e3f2",
-        highlightStroke: "#2a558c",
+        label: 'Voting Set One, Round One',
+        fillColor: '#0d0d0d',
+        strokeColor: '#5ab8f3',
+        highlightFill: '#a0e3f2',
+        highlightStroke: '#2a558c',
         data: []
       },
     ]
@@ -76,7 +76,7 @@ VoteTracker.prototype.populateChartData = function (pData) {
     pData.datasets[0].data[i] = imgArray[i].numberOfVotes;
   };
   return pData;
-}
+};
 
 VoteTracker.prototype.handleImgClicks = function (event) {
   var tagHandle = (event.currentTarget.parentElement.id == 'clickOne') ? 0 : 1;
@@ -122,13 +122,13 @@ VoteTracker.prototype.resetData = function (ev){
 
 VoteTracker.prototype.confirmData = function (ev) {
   localStorage.setItem('superKey', 'null');
-  document.getElementById('confirmButton').className = "hidden";
-  document.getElementById('cancel').className = "hidden";
+  document.getElementById('confirmButton').className = 'hidden';
+  document.getElementById('cancel').className = 'hidden';
 };
 
 VoteTracker.prototype.cancelReset = function (ev) {
-  document.getElementById('confirmButton').className = "hidden";
-  document.getElementById('cancel').className = "hidden";
+  document.getElementById('confirmButton').className = 'hidden';
+  document.getElementById('cancel').className = 'hidden';
 };
 
 function handleTheReset (event) {
